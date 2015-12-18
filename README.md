0cb9f9a4d1ea5d514f5a59aa11c7bffab9dc791b
Chris K <offero@wheeljack>
2015-12-18 10:40:43 -0500

    Initial commit and start of project.

---
+++ README.md
@@ -0,0 +1,4 @@
+# jsbamboo
+
+A reliable distributed redis-backed job queuing system with ack/fail semantics
+and support for multiple languages.
---
+++ test/test_job.js
@@ -0,0 +1,23 @@
+var job = require('../lib/job');
+
+function jobCmp(job1, job2){
+    return job1.id === job2.id
+        && job1.priority === job2.priority
+        && job1.payload === job2.payload;
+}
+
+describe("Job", function(){
+    it("Serializes to/from string array.", function(){
+        var j0 = {
+            id: '123',
+            priority: '1',
+            payload: 'stuff'
+        };
+        var jobStrArray = ['id', '123', 'priority', '1', 'payload', 'stuff'];
+        var j1 = job.fromStringArray(jobStrArray);
+        var jobStrArray2 = j1.toStringArray();
+        var j2 = job.fromStringArray(jobStrArray2);
+        assert.ok(jobCmp(j0, j1));
+        assert.ok(jobCmp(j1, j2));
+    });
+});
