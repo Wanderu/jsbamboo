7b63345a7aeae1ac5754ee6a1169245ccc32fbf9
Chris K <offero@wheeljack>
2015-12-20 00:07:24 -0500

    Started RJQ class with ioredis library backing.

--- test/test_job.js
+++ test/test_job.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 var job = require('../lib/job');
 
 function jobCmp(job1, job2){
@@ -10,12 +11,12 @@ describe("Job", function(){
     it("Serializes to/from string array.", function(){
         var j0 = {
             id: '123',
-            priority: '1',
+            priority: 1,
             payload: 'stuff'
         };
         var jobStrArray = ['id', '123', 'priority', '1', 'payload', 'stuff'];
         var j1 = job.fromStringArray(jobStrArray);
-        var jobStrArray2 = j1.toStringArray();
+        var jobStrArray2 = job.toStringArray(j1);
         var j2 = job.fromStringArray(jobStrArray2);
         assert.ok(jobCmp(j0, j1));
         assert.ok(jobCmp(j1, j2));
