2fde0c04d219aa5b12df516c925bc9fada5204b1
Chris K <offero@wheeljack>
2015-12-21 11:57:06 -0500

    Modified job object conversion. More tests. Added consume().

--- test/test_job.js
+++ test/test_job.js
@@ -20,5 +20,26 @@ describe("Job", function(){
         var j2 = job.fromStringArray(jobStrArray2);
         assert.ok(jobCmp(j0, j1));
         assert.ok(jobCmp(j1, j2));
+        assert.ok(job.isValid(j1));
+        assert.ok(job.isValid(j2));
+    });
+
+    it("Serializes to/from objects or string arrays", function() {
+        var j0 = {
+            id: '123',
+            priority: 1,
+            payload: 'stuff'
+        };
+        var jobStrArray = ['id', '123', 'priority', '1', 'payload', 'stuff'];
+        var j1 = job.toJob(jobStrArray);
+        var jobStrArray2 = job.toStringArray(j1);
+        var j2 = job.toJob(jobStrArray2);
+        var j3 = job.toJob(j0);
+        assert.ok(jobCmp(j0, j1));
+        assert.ok(jobCmp(j1, j2));
+        assert.ok(jobCmp(j0, j3));
+        assert.ok(job.isValid(j1));
+        assert.ok(job.isValid(j2));
+        assert.ok(job.isValid(j3));
     });
 });
